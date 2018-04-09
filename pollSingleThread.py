9b57c3ef7da906ea74a71d2eb40c001522c2c7a8
sjacobs <sjacobs@dhcp-053217.ics.uci.edu>
2018-04-09 12:22:13 -0700

    Fixed Poller

--- pollSingleThread.py
+++ pollSingleThread.py
@@ -4,6 +4,7 @@ import sys, os
 import threading
 import time
 import pycurl
+import json
 from time import sleep
 
 try:
@@ -28,13 +29,17 @@ while (j<iterations):
                 trimmedLine = line.split("\"")
                 location = trimmedLine[7]
                 location = location.replace(" ","%20")
-                command = "http://promethium.ics.uci.edu:19002/sqlpp?aql=select%20r%2Cshelters%20from%20steven.Reports%20r"
-                command += "%20let%20shelters%20=%20(select%20s.location%20from%20steven.Shelters%20s%20where%20spatial_intersect(s.location%2Ccircle(\""
-                command += location + "\")))"
-                command += "%20where%20r.timeStamp%20>%20current_datetime()%20-%20day_time_duration(\"PT10S\")"
-                command += "%20and%20spatial_intersect(r.location%2Ccircle(\""
-                command += location + "\"))%3B"
-                c.setopt(c.URL, command)
+                url="http://promethium.ics.uci.edu:19002/query/service"
+                command = "select%20r%2Cshelters%20from%20steven.Reports%20r"
+                command += "%20let%20shelters%20=%20(select%20s.location%20from%20steven.Shelters%20s%20where%20spatial_intersect(s.location%2Ccircle('"
+                command += location + "')))"
+                command += "%20where%20r.timeStamp%20>%20current_datetime()%20-%20day_time_duration('PT10S')"
+                command += "%20and%20spatial_intersect(r.location%2Ccircle('"
+                command += location + "'))%3B"
+                data=json.dumps({"statement":command})
+                c.setopt(pycurl.URL,url)
+                c.setopt(pycurl.POST, 1)
+                c.setopt(pycurl.POSTFIELDS, data)
                 c.perform()
                 print('time: %f' % c.getinfo(c.TOTAL_TIME))
                 elapsed = time.time() - start
